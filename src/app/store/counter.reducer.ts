d854bddfe4521c40cfe2521dbd9a6fd96fe98d5f
Mohit Rathi <mohitrathi176@gmail.com>
2024-06-29 15:26:26 +0530

    Define reducer without createReducer

--- src/app/store/counter.reducer.ts
+++ src/app/store/counter.reducer.ts
@@ -1,10 +1,20 @@
-import { createReducer, on } from "@ngrx/store";
-import { decrement, increment } from "./counter.actions";
+// import { createReducer, on } from "@ngrx/store";
+// import { decrement, increment } from "./counter.actions";
 
 const initialState = 0;
 
-export const counterReducer = createReducer(
-	initialState,
-	on(increment, (state, action) => state + action.value),
-	on(decrement, (state, action) => state - action.value)
-);
+// export const counterReducer = createReducer(
+// 	initialState,
+// 	on(increment, (state, action) => state + action.value),
+// 	on(decrement, (state, action) => state - action.value)
+// );
+
+export function counterReducer(state = initialState, action: any) {
+	switch (action.type) {
+		case '[Counter] Increment':
+			return state + action.value;
+		case '[Counter] Decrement':
+			return state - action.value;
+	}
+	return state;
+}
